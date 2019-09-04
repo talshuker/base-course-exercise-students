224b92bb31609c8083f16f9fd60d232c4808a99c
alonk <alonk@mamdas.iaf>
2019-09-04 13:56:59 +0300

    Add "Create a launch" button + Documentation

--- src/main/java/iaf/ofek/hadracha/base_course/web_server/AirSituation/SimulativeAirSituationProvider.java
+++ src/main/java/iaf/ofek/hadracha/base_course/web_server/AirSituation/SimulativeAirSituationProvider.java
@@ -114,14 +114,31 @@ public class SimulativeAirSituationProvider implements AirSituationProvider {
      * Generate a double between min (inclusive) and max (exclusive).
      */
     private double generateRandomDoubleInRangeWithNormalDistribution(double min, double max) {
+        // In case that min and max are wrong, flip them
         if (min > max) {
             double temp = min;
             min = max;
             max = temp;
         }
-        double num = random.nextGaussian() * (max - min) / 8 + (max + min) / 2;
+
+        // Java's Gaussian generator produces a normal distribution with mean 0 and standard variation 1,
+        // i.e. 68% of the results are in [-1..1] and 95% are in [-2..2].
+        // We use this as our base number, as it is (pseudo) random.
+        double baseNumber = random.nextGaussian();
+
+        // Inflate the "bell" to the relevant range. Then it is divided by 8 because:
+        //  - by 2 because [-1..1] is a range of size 2
+        //  - by 2 to increase the number of results in range from 68% to 95%
+        //  - by 2 to increase the number of results in range from 95% to 99.9%
+        // Total: 2*2*2 = 8
+        double num = baseNumber * (max - min) / 8;
+        // Move mean to mid-range
+        num+= (max + min) / 2;
+
+        // All the results outside the limits will be moved to the limit
         num = Math.max(min+0.1, num);
         num = Math.min(max-0.1, num);
+
         return num;
     }
 
